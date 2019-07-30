fa03cb5d663bed0c16128d235ada9244ccd733b4
GTech1256 <pomah156@gmail.com>
2019-07-30 17:00:33 +0300

    Added styles for Slider.vue

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,14 +1,24 @@
 <template>
-  <swiper class="swiper" :options="swiperOption" ref="mySwiper">
-    <swiper-slide></swiper-slide>
+  <swiper 
+    class="swiper" 
+    :options="swiperOption"
+    :initialSlide="4" 
+    ref="mySwiper"
+  >
+    <swiper-slide>
+      <img src="@/assets/img/1.jpg">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="@/assets/img/2.jpg">
+    </swiper-slide>
 
     <div class="swiper-pagination" slot="pagination"></div>
-    <div class="swiper-button swiper-button-prev" slot="button-prev">
-       //svg
-    </div>
-    <div class="swiper-button swiper-button-next" slot="button-next">
-      //svg
-    </div>
+    <button class="swiper-button swiper-button-prev" slot="button-prev" type="button">
+       Предыдущий слайд
+    </button>
+    <button class="swiper-button swiper-button-next" slot="button-next" type="button">
+      Следующий слайд
+    </button>
   </swiper>
 </template>
 
@@ -44,5 +54,27 @@ export default {
 <style scoped lang="stylus">
 .swiper-container
   height 100%
-</style>
 
+.swiper-button 
+  display block
+  width 90px
+  height 83px
+  margin-top -42px
+
+  font-size 0
+
+  background: transparent url("../assets/svg/arrow.svg") no-repeat;
+  border: none 
+
+  &-prev
+    margin-left: 8px
+    transform rotate(180deg)
+  &-next
+    margin-right: 8px
+
+.swiper-pagination 
+  margin-bottom 20px
+
+  font-size 18px
+  letter-spacing: -1px
+</style>
